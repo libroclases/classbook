46227b61ec6958ed2e58dee76fe78943f595bb4e
Ulises Vergara <ulises108@gmail.com>
2024-03-04 10:36:16 -0300

    implementacion matriculas

--- nglibro15/src/app/tablas/matricula-alumno/matricula-alumno.component.ts
+++ nglibro15/src/app/tablas/matricula-alumno/matricula-alumno.component.ts
@@ -55,27 +55,11 @@ export class MatriculaAlumnoComponent implements OnInit {
     this.height = event.target.innerHeight - (this.banner_height + this.menu_height) + 'px';
   }
 
-  detalle_estado:any = {
-        alumno : { titulo: "Matrícula - Datos Alumno", fase: "Paso 1/3" },
-        apoderado : { titulo: "Matrícula - Datos Apoderado", fase: "Paso 2/3" },
-        matricula : { titulo: "Matrícula - Datos Matrícula", fase: "Paso 3/3" }
-      }
+  
+  ngOnInit() {}
 
-  ngOnInit() {
-
-    sessionStorage.setItem('Entidad', 'alumno');
-    this.modalDataObj = modalDataObject[lowerUpperTables[this.getEntidad()]];
-
-    sessionStorage.setItem('Estado', 'pre-consulta');
-    sessionStorage.setItem('Accion', 'buscar');
-  }
-
-  getEntidad(): any  { return sessionStorage.getItem('Entidad')}
-  getEstado():  any  { return sessionStorage.getItem('Estado')}
-  getAccion():  any  { return sessionStorage.getItem('Accion')}
-
-  setSession(tipo:string, valor:string) { sessionStorage.setItem(tipo, valor)  }
-
+  
+  
   constructor(
     private mensaje: MessageService,
     private crud: CrudService,
@@ -109,8 +93,18 @@ export class MatriculaAlumnoComponent implements OnInit {
       })
 
       this.formConsulta = new FormGroup({
-      rut_entidad : new FormControl('', rutValidator())
-
+          rut : new FormControl('', rutValidator()),
+/*          nombre: new FormControl('',Validators.required),
+          apellido1: new FormControl('',Validators.required),
+          apellido2: new FormControl('',Validators.required),
+          direccion: new FormControl('',Validators.required),
+          celular: new FormControl('',Validators.required),
+          nacimiento: new FormControl('',Validators.required),
+          Sexo: new FormControl('',Validators.required),
+          Region: new FormControl('',Validators.required),
+          Provincia: new FormControl('',Validators.required),
+          Comuna: new FormControl('',Validators.required)
+*/          
     })
 
 
@@ -125,9 +119,7 @@ export class MatriculaAlumnoComponent implements OnInit {
   cancelar() {
     this.formConsulta.reset();
     this.muestra_dialog = false;
-    this.setSession('Entidad','alumno');
-    this.setSession('Estado','pre-consulta');
-    this.setSession('Accion','buscar')
+  
     this.mensaje.nextMsg({})
 
   }
@@ -142,69 +134,15 @@ export class MatriculaAlumnoComponent implements OnInit {
       // console.log('2.- entidad',this.getEntidad());
       // console.log('2.- data',e.data);
 
-      if (this.getEntidad()=='alumno') { this.r_alumno = e.data;}
-      if (this.getEntidad()=='apoderado') { this.r_apoderado = e.data;}
     }
 
     this.msg= e;
 
-    this.reset();
-    if (this.getEntidad() == 'alumno') {
-      this.setSession('Entidad','apoderado');
-      this.setSession('Estado','pre-consulta');
-      this.setSession('Accion','buscar')
-
-    }
-    else if (this.getEntidad() == 'apoderado') {
-      this.setSession('Entidad','matricula');
-      this.setSession('Estado','mostrar')
-      this.setSession('Accion','generar')
-
-    } else if (this.getEntidad() == 'matricula') {
-      this.setSession('Entidad','alumno');
-      this.setSession('Estado','pre-consulta')
-      this.setSession('Accion','buscar')
-
-    }
-
+ 
   }
 
   matricula:any={};
 
-  mostra_matricula() {
-
-    this.setSession('Entidad','matricula')
-    this.setSession('Estado','generado')
-    this.setSession('Accion','ingresar')
-
-    var tablas:any = {};
-    var textos:any = {};
-    var ocultos:any = {};
-    var fechas:any = {};
-
-    const modaldata: any = modalDataObject[lowerUpperTables[this.getEntidad()]];
-
-    modaldata.textFields.forEach((tex: string) => textos[tex] = null);
-    modaldata.dateFields.forEach((date: string) => fechas[date] = null);
-
-    tablas['colegio'] = { id: 0};
-    tablas['apoderado'] = { id: this.r_apoderado.id };
-    tablas['alumno'] = { id: this.r_alumno.id };
-    tablas['curso'] = { id: 0 };
-    tablas['anno'] = { id: 0 };
-    tablas['vinculo'] = { id: 0 };
-
-    ocultos['alumno'] = this.r_alumno.id;
-    ocultos['apoderado'] = this.r_apoderado.id;
-
-    let msg:any = { registro : { id:0,  tablas, textos, ocultos, fechas } ,
-                    tabla: lowerUpperTables[this.getEntidad()]
-                  }
-    console.log('msg', msg);
-    this.mensaje.nextMsg(msg);
-    this.muestra_dialog = true;
-
-  }
 
 
   rut_consulta() {
@@ -222,57 +160,9 @@ export class MatriculaAlumnoComponent implements OnInit {
       return output
     };
 
-    const getData = (data: any) : void => {
+   
+   
 
-        let modaldata: any = modalDataObject[lowerUpperTables[this.getEntidad()]];
-
-        if (data) {
-
-              if (this.getEntidad()=='alumno') {  this.r_alumno = data; }
-              if (this.getEntidad()=='apoderado') {  this.r_apoderado = data; }
-
-              let msg:any = { registro : {
-                id: data.id,
-                tablas: getModalData('tablas', modaldata.tables, data),
-                textos: getModalData('textos',modaldata.textFields,data),
-                ocultos: getModalData('ocultos',modaldata.hidden,data),
-                fechas: getModalData('fechas',modaldata.dateFields,data),
-                },
-                tabla: lowerUpperTables[this.getEntidad()]
-              }
-
-              // console.log('msg->',msg);
-
-              this.mensaje.nextMsg(msg);
-
-              this.setSession('Estado', 'encontrado');
-              this.setSession('Accion', 'verificar');
-              this.muestra_dialog = true;
-      } else {
-
-
-          let msg:any = { registro : {
-            id: 0,
-            tablas: getModalData('tablas', modaldata.tables),
-            textos: getModalData('textos',modaldata.textFields),
-            ocultos: getModalData('ocultos',modaldata.hidden),
-            fechas: getModalData('fechas',modaldata.dateFields),
-
-
-          },
-          rut: this.formConsulta.value.rut_entidad,
-          tabla: lowerUpperTables[this.getEntidad()]}
-
-          // console.log('msg->',msg)
-          this.mensaje.nextMsg(msg)
-          this.muestra_dialog = true;
-       }
-      }
-
-    this.crud.getByRut(this.getEntidad(), this.formConsulta.value.rut_entidad).pipe(
-      tap(data => getData(data))
-    )
-    .subscribe()
   }
 
 
