326e5650bdc69e688cfc72683f8f743eab068ece
Ulises Vergara <ulises108@gmail.com>
2024-03-06 10:21:07 -0300

    se agrega soporte para pruebas unitarias

---
+++ nglibro15/src/app/shared/directives/profe-validator/profe-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ProfeValidatorsDirective } from './profe-validator.directive';
+
+describe('ProfeValidatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ProfeValidatorsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
