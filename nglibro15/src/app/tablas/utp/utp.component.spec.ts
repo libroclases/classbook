40668cc8107af12bed7ceb772bee798619b2b436
Ulises Vergara <ulises108@gmail.com>
2024-03-12 14:34:35 -0300

    pruebas unitarias

---
+++ nglibro15/src/app/tablas/utp/utp.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+
+import { UtpComponent } from './utp.component';
+import { MaintainerComponent } from 'src/app/shared/componentes/maintainer/maintainer.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MockComponent, MockedComponentFixture, MockRender } from 'ng-mocks';
+
+describe('UtpComponent', () => {
+  let component: UtpComponent;
+  let fixture: MockedComponentFixture<UtpComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ 
+        UtpComponent,
+        MockComponent(MaintainerComponent),       
+      ],
+      providers:[ ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    })
+    .compileComponents();
+
+    fixture = MockRender(UtpComponent);
+    component = fixture.point.componentInstance;
+    fixture.detectChanges();
+  });
+
+  fit('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
