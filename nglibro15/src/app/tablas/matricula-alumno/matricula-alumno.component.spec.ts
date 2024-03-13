9eca8097ed9ac3feae86b0f836a1cdf344349b15
Ulises Vergara <ulises108@gmail.com>
2024-03-13 17:44:25 -0300

    pruebas unitarias

--- nglibro15/src/app/tablas/matricula-alumno/matricula-alumno.component.spec.ts
+++ nglibro15/src/app/tablas/matricula-alumno/matricula-alumno.component.spec.ts
@@ -1,19 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { MatriculaAlumnoComponent } from './matricula-alumno.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MockComponent, MockRender, MockedComponentFixture } from 'ng-mocks';
+
 
 describe('MatriculaAlumnoComponent', () => {
   let component: MatriculaAlumnoComponent;
-  let fixture: ComponentFixture<MatriculaAlumnoComponent>;
+  let fixture: MockedComponentFixture<MatriculaAlumnoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MatriculaAlumnoComponent ]
+      declarations: [ MockComponent(MatriculaAlumnoComponent) ],
+      imports: [
+        HttpClientModule,
+      ], 
+      providers:[],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MatriculaAlumnoComponent);
-    component = fixture.componentInstance;
+    fixture = MockRender(MatriculaAlumnoComponent);
+    component = fixture.point.componentInstance;
     fixture.detectChanges();
   });
 
