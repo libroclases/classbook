1b5ac966c9f96673c70fc54e4de3ff5d558619ec
Ulises Vergara <ulises108@gmail.com>
2024-03-19 11:28:57 -0300

    profe validator

--- nglibro15/src/app/shared/directives/profe-validator/profe-validator.directive.spec.ts
+++ nglibro15/src/app/shared/directives/profe-validator/profe-validator.directive.spec.ts
@@ -1,8 +1,11 @@
 import { ProfeValidatorsDirective } from './profe-validator.directive';
+import { MessageService } from '../../services/message/message.service';
+import { Inject } from '@angular/core';
 
 describe('ProfeValidatorDirective', () => {
+  var ms = Inject(MessageService);
   it('should create an instance', () => {
-    const directive = new ProfeValidatorsDirective();
+    const directive = new ProfeValidatorsDirective(ms);
     expect(directive).toBeTruthy();
   });
 });
