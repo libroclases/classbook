2f9b0df6e2f5e8691539bb1f1b35e1e7ac4245e6
Ulises Vergara <ulises108@gmail.com>
2024-03-30 15:28:00 -0300

    implementacion ngxs

--- nglibro15/src/app/app.module.ts
+++ nglibro15/src/app/app.module.ts
@@ -64,7 +64,7 @@ import { ProfeValidatorsDirective } from './shared/directives/profe-validator/pr
 import { MessageService } from './shared/services/message/message.service';
 
 import { NgxsModule } from '@ngxs/store';
-import { AlumnoState } from './ngxs/store/alumno.state';
+import { AlumnoState } from '../app/ngxs/alumno/state/alumno.state';
 
 // import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 
--- nglibro15/src/app/tablas/alumno/alumno.component.ts
+++ nglibro15/src/app/tablas/alumno/alumno.component.ts
@@ -1,14 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable, Subject } from 'rxjs';
+import { GetAlumno } from 'src/app/ngxs/alumno/state/alumno.actions';
+import { AlumnoState } from 'src/app/ngxs/alumno/state/alumno.state';
 
 @Component({
   selector: 'app-alumno',
-  templateUrl: './../../shared/componentes/maintainer-template/maintainer-template.component.html',
+  templateUrl: './alumno.component.html',
   styleUrls: ['./alumno.component.css']
 })
 export class AlumnoComponent implements OnInit {
+ // Selector asociado a la propiedad people del estado
+ @Select(AlumnoState.alumno)
+ alumno$!: Observable<any>;
+
+ searchTerm$ = new Subject<string>();
+
+ // Array donde almacenaremos las personas
+ public alumnoFiltered: any = [];
+
+ // Atributo para el filtro del nombre
+ public filterName: string;
+ 
+ constructor(private store: Store) {
+   this.filterName = '';
+ }
+
+ search(event: Event): void {
+  console.log('poronga');
+  const element = event.currentTarget as HTMLInputElement;
+  this.searchTerm$.next(element.value);
+  this.filterName=element.value;
+  this.filter()
+}
+
+ ngOnInit() {
+   this.fetchAlumno();
+ }
+
+
+ fetchAlumno(){
+   // Nos subscribimos para estar pendiente de los cambios de la propiedad
+   this.alumno$.subscribe({
+     next: () => {
+       // Obtenemos el array de personas de la store
+       this.alumnoFiltered = this.store.selectSnapshot(AlumnoState.alumno);
+       console.log('Alumno ha cambiado', this.store.snapshot());
+     }
+   })
+ }
+
+ filter() {
+   // Activo la acción, dandole el nombre a filtrar
+   console.log('filterName', this.filterName)
+   this.store.dispatch(new GetAlumno({ nombre: this.filterName }));
+   
+ }
+
+
 
   // Tabla Principal
-
+  /*
   mainTable: string = 'alumno';
   tableTitle = 'Alumnos';
   textFields = ['apellido1','apellido2','nombre', 'rut', 'direccion', 'celular'];
@@ -37,5 +89,5 @@ export class AlumnoComponent implements OnInit {
     this.changeFnsArray = [ this.emptyFn, this.emptyFn ];
 
   }
-
+  */
 }
