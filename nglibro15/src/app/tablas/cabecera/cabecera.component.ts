008d54db028f15e1ea7e802aa35418af0fd9a54e
Ulises Vergara <ulises108@gmail.com>
2024-05-01 10:40:55 -0400

    eliminacion de auth en cabecera

--- nglibro15/src/app/tablas/cabecera/cabecera.component.ts
+++ nglibro15/src/app/tablas/cabecera/cabecera.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment as env } from '../../../environments/environment';
 import { AuthService } from '@auth0/auth0-angular';
-import { Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { UsuarioState } from 'src/app/ngxs/usuario.state';
 import { Usuario } from 'src/app/ngxs/usuario.model';
+import { GetUsuario } from 'src/app/ngxs/usuario.actions';
 
 @Component({
   selector: 'app-cabecera',
@@ -21,43 +22,34 @@ export class CabeceraComponent {
 
    @Select(UsuarioState.usuario) usuario$!: Observable<Usuario>;
 
-  constructor( private auth: AuthService) {
+  
+  constructor() {
+    const getColor = (color:string | null) => {
+          if (color=='azul' || !color) { console.log('color',color)
+            this.color = this.objcolors.azul.color;
+            this.lineal = this.objcolors.azul.lineal;
+            }
+          else if (color=='verde') {
+            this.color = this.objcolors.verde.color;
+            this.lineal = this.objcolors.verde.lineal;
 
-    this.auth.isAuthenticated$.subscribe(isAuth => { if(isAuth) {
-      console.log('estoy autentificado');
-      this.usuario$.subscribe( (info:any)  => {
-        if (info[0].personalInfo) {
-
-          localStorage.setItem('Color', info.personalInfo.usuario.Tema.nombre);
-          getColor(info.personalInfo.usuario.Tema.nombre);
-          this.fullName = (info[0].personalInfo.datos_persona) ? Object.values(info[0].personalInfo.datos_persona).slice(1).toString(): '';
-        }
-
-      }) ;
-     } else {
-        this.fullName="";
-        getColor(localStorage.getItem('Color'))
+          }
+          else if (color=='naranjo') {
+            this.color = this.objcolors.naranjo.color;
+            this.lineal = this.objcolors.naranjo.lineal;
+          }
+    }
+    this.usuario$.subscribe((info:any) => {
+      if (info.personalInfo) {
+        getColor(info.personalInfo.usuario.Tema.nombre);
+        console.log('poronga',info.personalInfo);
+        this.fullName = (info.personalInfo.datos_persona) ? Object.values(info.personalInfo.datos_persona).slice(1).toString(): ''; 
+      }
+      else { 
+        getColor(localStorage.getItem('Color'));
       }
     })
-
-    const getColor = (color:string | null) => {
-
-      if (color=='azul' || !color) { console.log('color',color)
-        this.color = this.objcolors.azul.color;
-        this.lineal = this.objcolors.azul.lineal;
-       }
-      else if (color=='verde') {
-        this.color = this.objcolors.verde.color;
-        this.lineal = this.objcolors.verde.lineal;
-
-      }
-      else if (color=='naranjo') {
-        this.color = this.objcolors.naranjo.color;
-        this.lineal = this.objcolors.naranjo.lineal;
-      }
-
-    }
-
   }
+
 
 }
